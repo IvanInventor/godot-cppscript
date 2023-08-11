95977ba8f044af48ea0eec5225b8d2b9ee81adff
Vano <kavo@tavo.mail.com>
2023-08-11 15:31:14 +0300

    changed structure

---
+++ src/macros.h
@@ -0,0 +1,13 @@
+#define REGISTER_CLASS_GEN_CODE(CLASS_NAME) REGISTER_CLASS_GEN_CODE_CLASS_ ## CLASS_NAME
+#define STR(s) # s
+#define EXPORT_METHOD(METHOD_NAME, ...)
+
+#define GCLASS(CLASS_NAME, CLASS_NAME_INH) 						\
+	GDCLASS(CLASS_NAME , CLASS_NAME_INH)			\
+public:								\
+static void _bind_methods();
+
+#define GPROPERTY(...)
+#define GMETHOD(...)
+#define GGROUP(...)
+#define GSUBGROUP(...)
---
+++ src/register_types.cpp
@@ -0,0 +1,42 @@
+
+#include <gdextension_interface.h>
+
+#include <godot_cpp/core/class_db.hpp>
+#include <godot_cpp/core/defs.hpp>
+#include <godot_cpp/godot.hpp>
+
+//temp
+#include <godot_cpp/classes/sprite2d.hpp>
+#include "register_types.h"
+
+using namespace godot;
+#include "scripts.gen.h"
+
+
+void initialize_example_module(ModuleInitializationLevel p_level) {
+	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
+		return;
+	}
+	// Write your custom code before or after this macro
+	register_script_classes();
+
+}
+
+void uninitialize_example_module(ModuleInitializationLevel p_level) {
+	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
+		return;
+	}
+}
+
+extern "C" {
+// Initialization.
+GDExtensionBool GDE_EXPORT example_library_init(const GDExtensionInterface *p_interface, GDExtensionClassLibraryPtr p_library, GDExtensionInitialization *r_initialization) {
+	godot::GDExtensionBinding::InitObject init_obj(p_interface, p_library, r_initialization);
+
+	init_obj.register_initializer(initialize_example_module);
+	init_obj.register_terminator(uninitialize_example_module);
+	init_obj.set_minimum_library_initialization_level(MODULE_INITIALIZATION_LEVEL_SCENE);
+
+	return init_obj.init();
+}
+}
