f6cc4293d199672720eb2a710b66a6da7226131a
Vano <whoasked@tutanota.com>
2024-03-06 19:17:36 +0200

    SNAME macro

--- src/cppscript_defs.h
+++ src/cppscript_defs.h
@@ -60,4 +60,12 @@ static inline s_type& s_name = *reinterpret_cast<s_type*>(&s_name ## _impl);
 namespace impl {
 struct StaticAccess;
 };
+
+/* Similar to Godot engine's SNAME macro idea or GDScript's `&"string_name"` syntax,
+ * it creates static instance of StringName and returns reference to it.
+ * It guarantees that `StringName` exists before calling this lambda,
+ * but not that `StringName`s with same string literal are the same object.
+ */
+#define SNAME(str_literal) ([]() -> const ::godot::StringName& {static const ::godot::StringName str(str_literal); return str;}())
+
 #endif // CPPSCRIPT_HEADER
