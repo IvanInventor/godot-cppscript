adc58490f2396877b414b2a1a2641094e90db652
Vano <whoasked@tutanota.com>
2024-10-08 15:29:49 +0300

    allow no commit on current branch

--- generate.sh
+++ generate.sh
@@ -2,7 +2,7 @@
 # Helper to generate + commit to both dev branch and master
 MASTER_DIR="$1"
 ./generate.py "$MASTER_DIR" || exit 1
-git commit -a || exit 1
+git commit -a || true
 
 REF_SHORT=$(git rev-parse @ | head -c8)
 
