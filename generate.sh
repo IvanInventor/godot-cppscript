d30fd3520acf55c541c18306c1a4a41f85833265
Vano <whoasked@tutanota.com>
2024-10-25 14:50:21 +0300

    better generate script, template file

--- generate.sh
+++ generate.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
 # Helper to generate + commit to both dev branch and master
 MASTER_DIR="$1"
-./generate.py "$MASTER_DIR" || exit 1
-if ! git diff --cached --quiet; then
+if [ "$MASTER_DIR" == "" ]; then
+	echo "Usage: "
+	echo "\t$0" "<master_worktree_path>"
+	exit 1
+fi
+
+if git diff --cached --quiet; then
 	echo "No changes"
 	exit 0
 fi
 
-git commit -a || true
+./generate.py "$MASTER_DIR" || exit 1
+
+git commit -a || exit 1
 
 REF_SHORT=$(git rev-parse @ | head -c8)
 
 cd "$MASTER_DIR"
-git diff --cached --quiet || git commit -a -m "Sync with $REF_SHORT" || exit 1
+if git diff --cached --quiet ; then
+	echo "Cannot commit to" "'$MASTER_DIR'" ": no changes"
+	exit 1
+else
+	git commit -a -m "Sync with $REF_SHORT"
+fi
